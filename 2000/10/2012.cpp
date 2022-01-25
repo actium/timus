94bb830f876058c61d988a7df629c649b60c3cef
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2022-01-25 23:10:07 +0300

    Add solution for problem 2012

---
+++ 2000/10/2012.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    unsigned f;
+    std::cin >> f;
+
+    if (f + 5 >= 12)
+        std::cout << "YES" << '\n';
+    else
+        std::cout << "NO" << '\n';
+
+    return 0;
+}
