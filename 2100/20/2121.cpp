d73e0a0e05f9f993d053e5833c9f933713ca4249
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-11-07 21:00:38 +0300

    Add solution for problem 2121

---
+++ 2100/20/2121.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    using integer = unsigned long long;
+
+    integer a;
+    std::cin >> a;
+
+    std::cout << 4 * a - 1 << ".6666666667" << '\n';
+
+    return 0;
+}
