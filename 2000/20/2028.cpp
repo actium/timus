b705bba57572c607972388fb52269d49c2fafa0f
Dmitry Dolgopolov <5691735+actium@users.noreply.github.com>
2023-11-20 13:29:33 +0300

    Add solution for problem 2028

---
+++ 2000/20/2028.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    std::cout << "11 3\n";
+    std::cout << "v#.\n";
+    std::cout << "?!.\n";
+    std::cout << ".S.\n";
+    std::cout << "v.<\n";
+    std::cout << ">@.\n";
+    std::cout << ">v.\n";
+    std::cout << ".S.\n";
+    std::cout << "N+.\n";
+    std::cout << "+S.\n";
+    std::cout << "N-N\n";
+    std::cout << "^@^\n";
+
+    return 0;
+}
